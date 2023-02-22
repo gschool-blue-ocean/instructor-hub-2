69757c0ae01eead7b249a5ae1303687d11dce7ac
sds3000 <64442811+sds3000@users.noreply.github.com>
2023-02-22 16:26:04 -0500

    test

--- api/express.js
+++ api/express.js
@@ -2,7 +2,7 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
-require('dotenv').config();
+require('dotenv').config()
 
 const { Pool } = require('pg');
 
